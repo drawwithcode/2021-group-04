d27708a11d0a0a34335ca425395d7cdceb219ea7
Andrea Benedetto <34688096+benedettoandrea@users.noreply.github.com>
2022-01-14 12:56:10 +0100

    transitions prototype

--- questions.js
+++ questions.js
@@ -1,5 +1,22 @@
-//temporary tests
 let intro = document.getElementById("intro");
-intro.style.display = "none";
+let questions = document.getElementById("questions");
+let identity = document.getElementById("identity");
+let I1 = document.getElementById("I1");
+let relationships = document.getElementById("relationships");
+let violence = document.getElementById("violence");
+let sexuality = document.getElementById("sexuality");
+let beliefs = document.getElementById("beliefs");
 let artwork = document.getElementById("artwork");
-artwork.style.display = "none";
+
+function nextSection(currentQuestion) {
+  switch (currentQuestion) {
+    case "1":
+      intro.className = "sctn hide";
+      setTimeout(() => {
+        questions.className = "sctn show";
+        identity.className = "sctn show";
+        I1.className = "container show";
+      }, 750);
+    default:
+  }
+}
